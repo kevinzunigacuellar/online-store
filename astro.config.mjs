9da12b1b86f17dc6399b1587a138487386f2aeb9
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-05-15 15:02:05 -0400

    add solidjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/functions";
+import solidJs from "@astrojs/solid-js";
 
+// https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  integrations: [solidJs()]
 });
